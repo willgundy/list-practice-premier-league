a22fa0f99f9b3b0ee319f961d594620ea360675d
Will Gundy <willgunderson503@gmail.com>
2022-03-15 13:05:03 -0700

    Finished html and css work

--- app.js
+++ app.js
@@ -6,3 +6,12 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+
+  // let state
+
+// set event listeners 
+
+const headerButtons = document.querySelectorAll('.headerButtons');
+headerButtons.forEach(button => button.addEventListener('mouseover', function(){button.classList.add('button-hover');}));
+headerButtons.forEach(button => button.addEventListener('mouseout', function(){button.classList.remove('button-hover');}));
